e377ecc6144723ac9bbae00f78ffc77ddfc8f891
BitaShamsafar <bita.shamsafar@hotmail.com>
2026-02-10 09:59:42 +0100

    naming typo fixed

---
+++ backend/src/main/java/org/example/backend/repositories/HotelRepository.java
@@ -0,0 +1,16 @@
+package org.example.backend.repositories;
+
+import org.example.backend.models.Hotel;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface HotelRepository extends MongoRepository<@NotNull Hotel, @NotNull String> {
+    @NotNull Optional<Hotel> findById(String id);
+    List<Hotel> findByName(String name);
+    List<Hotel> findFirstByRating(int rating);
+}
